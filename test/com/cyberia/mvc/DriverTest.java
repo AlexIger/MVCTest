c3da595a565df24ce87ea104c0f1da531d10d365
AlexIger <cyberia@protonmail.com>
2023-10-01 20:15:27 +0700

    Init commit Master
    Signed-off-by: AlexIger <cyberia@protonmail.com>

--- test/com/cyberia/mvc/DriverTest.java
+++ test/com/cyberia/mvc/DriverTest.java
@@ -4,6 +4,12 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import static com.google.common.truth.Truth.assertThat;
+import static com.google.common.truth.Truth.assertWithMessage;
+
+// for assertions on Java 8 types (Streams and java.util.Optional)
+import static com.google.common.truth.Truth8.assertThat;
+
 class DriverTest {
 
     @Test
@@ -11,7 +17,11 @@ class DriverTest {
     {
         Driver driver  = new Driver();
 
+        String num = "110";
        assertEquals(110, driver.getAge(10));
+
+        assertThat(num).contains("10");
+
 
     }
 }
