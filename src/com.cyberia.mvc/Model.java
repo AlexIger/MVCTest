c44db5f93b1c7b0564466962c80c884961da8506
AlexIger <cyberia@protonmail.com>
2023-10-01 20:26:59 +0700

    Init commit Master
    Signed-off-by: AlexIger <cyberia@protonmail.com>

--- src/com.cyberia.mvc/Model.java
+++ src/com.cyberia.mvc/Model.java
@@ -5,6 +5,7 @@ public class Model
     public static RecordStudent getStudent()
     {
         // New record
+        //new new record
         return  new RecordStudent();
     }
 
--- test/com/cyberia/mvc/DriverTest.java
+++ test/com/cyberia/mvc/DriverTest.java
@@ -13,12 +13,11 @@ import static com.google.common.truth.Truth8.assertThat;
 class DriverTest {
 
     @Test
-    void test_getAge_return_int()
-    {
-        Driver driver  = new Driver();
+    void test_getAge_return_int() {
+        Driver driver = new Driver();
 
         String num = "110";
-       assertEquals(110, driver.getAge(10));
+        assertEquals(110, driver.getAge(10));
 
         assertThat(num).contains("10");
 
